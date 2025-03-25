65b4d817c4e4d9d09c7e52a54def4cc4279c877b
pipinotuc <marco.criveller1995@gmail.com>
2025-03-26 00:27:04 +0100

    Correggo struttura: ora index.html è nella root

--- giardino_dell_idea_potenziato/giardino-dell-idea-main/salva_data_e_seme.js
+++
@@ -1,7 +0,0 @@
-function salvaDataESemeAttuale(seme) {
-  const oggi = new Date().toLocaleDateString('it-IT', {
-    day: '2-digit', month: 'long', year: 'numeric'
-  });
-  localStorage.setItem('lastSeedDate', oggi);
-  localStorage.setItem('lastSeedName', seme);
-}
